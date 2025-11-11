26950de73f9e06c46a43d908502ed1082da51ca2
Mathieu <matcaballero03@gmail.com>
2025-11-10 22:28:43 -0500

    Versión inicial del módulo Paciente y Episodio

---
+++ build/classes/Main.rs
@@ -0,0 +1,1 @@
+hospital.pacienteepisodio.Main
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+package hospital.pacienteepisodio;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Sistema Hospitalario: Módulo Paciente y Episodio ===");
+        System.out.println("Preparado para registrar pacientes y manejar episodios médicos.");
+    }
+}
