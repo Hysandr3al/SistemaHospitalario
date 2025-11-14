3e579effe409a2ccc4eeca9c1c09203887969638
Mathieu <matcaballero03@gmail.com>
2025-11-14 10:08:15 -0500

    Eliminado Main.java antiguo y organización final del proyecto

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-package hospital.pacienteepisodio;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("=== Sistema Hospitalario: Módulo Paciente y Episodio ===");
-        System.out.println("Preparado para registrar pacientes y manejar episodios médicos.");
-    }
-}
